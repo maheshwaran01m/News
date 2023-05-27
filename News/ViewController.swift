b65fc4b447df9e651626e2c21c8cb0f78ea02b18
MAHESHWARAN <maheshwaran99m@gmail.com>
2023-05-27 16:30:49 +0530

    Inital Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by MAHESHWARAN on 27/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureView()
+  }
+  
+  private func configureView() {
+    title = "News"
+    view.backgroundColor = .systemBackground
+  }
+}
+
